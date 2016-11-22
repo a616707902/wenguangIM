8d34b597d824ab771d6548b412837f63aafe223f
a616707902 <616707902@qq.com>
2016-11-22 22:29:21 +0800

    增加联系人

--- WenGuang/app/src/main/java/com/wenguang/chat/fragment/ContactFragment.java
+++ WenGuang/app/src/main/java/com/wenguang/chat/fragment/ContactFragment.java
@@ -2,44 +2,160 @@ package com.wenguang.chat.fragment;
 
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.wenguang.chat.R;
+import com.wenguang.chat.adapter.ContactsSortAdapter;
 import com.wenguang.chat.base.BaseFragment;
 import com.wenguang.chat.mvp.presenter.BasePresenter;
+import com.wenguang.chat.mvp.presenter.ContactFragmentPresenter;
+import com.wenguang.chat.utils.common.CharacterParser;
+import com.wenguang.chat.utils.common.PinyinComparator;
+import com.wenguang.chat.utils.common.SortModel;
+import com.wenguang.chat.widget.SideBar;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ContactFragment extends BaseFragment {
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.home_fragment, null);
-		return view;
-	}
 
-	@Override
-	protected int getlayoutId() {
-		return 0;
-	}
+    @Bind(R.id.et_search)
+    EditText etSearch;
+    @Bind(R.id.ivClearText)
+    ImageView ivClearText;
+    @Bind(R.id.layoutContainer)
+    FrameLayout layoutContainer;
+    @Bind(R.id.dialog)
+    TextView dialog;
+    @Bind(R.id.sidrbar)
+    SideBar sideBar;
+    @Bind(R.id.lv_contacts)
+    ListView mListView;
 
-	@Override
-	protected void initInjector() {
+    /**
+     * 汉字转换成拼音的类
+     */
+    private CharacterParser characterParser;
 
-	}
+    /**
+     * 根据拼音来排列ListView里面的数据类
+     */
+    private PinyinComparator pinyinComparator;
 
-	@Override
-	protected void initEventAndData() {
+    private List<SortModel> mAllContactsList;
+    private ContactsSortAdapter adapter;
 
-	}
 
-	@Override
-	protected void lazyLoadData() {
+    @Override
+    protected int getlayoutId() {
+        return R.layout.contact_fragment;
+    }
 
-	}
+    @Override
+    protected void initInjector() {
+        characterParser = CharacterParser.getInstance();
+        mAllContactsList = new ArrayList<SortModel>();
+        pinyinComparator = new PinyinComparator();
+        Collections.sort(mAllContactsList, pinyinComparator);// 根据a-z进行排序源数据
+        adapter = new ContactsSortAdapter(mActivity, mAllContactsList);
+        mListView.setAdapter(adapter);
+    }
 
-	@Override
-	public BasePresenter getPresenter() {
-		return null;
-	}
+    @Override
+    protected void initEventAndData() {
+
+        /** 清除输入字符 **/
+        ivClearText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                etSearch.setText("");
+            }
+        });
+        etSearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable e) {
+
+                String content = etSearch.getText().toString();
+                if ("".equals(content)) {
+                    ivClearText.setVisibility(View.INVISIBLE);
+                } else {
+                    ivClearText.setVisibility(View.VISIBLE);
+                }
+                if (content.length() > 0) {
+                    ArrayList<SortModel> fileterList = (ArrayList<SortModel>) search(content);
+                    adapter.updateListView(fileterList);
+                    // mAdapter.updateData(mContacts);
+                } else {
+                    adapter.updateListView(mAllContactsList);
+                }
+                mListView.setSelection(0);
+
+            }
+
+        });
+
+        // 设置右侧[A-Z]快速导航栏触摸监听
+        sideBar.setOnTouchingLetterChangedListener(new SideBar.OnTouchingLetterChangedListener() {
+
+            @Override
+            public void onTouchingLetterChanged(String s) {
+                // 该字母首次出现的位置
+                int position = adapter.getPositionForSection(s.charAt(0));
+                if (position != -1) {
+                    mListView.setSelection(position);
+                }
+            }
+        });
+        // item事件
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long arg3) {
+                ContactsSortAdapter.ViewHolder viewHolder = (ContactsSortAdapter.ViewHolder) view.getTag();
+                viewHolder.cbChecked.performClick();
+                adapter.toggleChecked(position);
+            }
+        });
+
+    }
+
+    @Override
+    protected void lazyLoadData() {
+
+    }
+
+    @Override
+    public BasePresenter getPresenter() {
+        return new ContactFragmentPresenter();
+    }
+
+
+
 }
--- WenGuang/app/src/main/java/com/wenguang/chat/mvp/model/ContactFragmentModelImpl.java
+++ WenGuang/app/src/main/java/com/wenguang/chat/mvp/model/ContactFragmentModelImpl.java
@@ -1,10 +1,118 @@
 package com.wenguang.chat.mvp.model;
 
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.wenguang.chat.utils.common.SortModel;
+import com.wenguang.chat.utils.common.SortToken;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
 * Created by MVPHelper on 2016/11/16
 */
 
 public class ContactFragmentModelImpl implements ContactFragmentModel{
+    String chReg = "[\\u4E00-\\u9FA5]+";// 中文字符串匹配
 
+    /**
+     * 加载联系人数据
+     */
+    public  void loadContacts(final Context context) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // 插叙
+                    String queryTye[] = { ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER, "sort_key", "phonebook_label",
+                            ContactsContract.CommonDataKinds.Phone.PHOTO_ID };
+                    ContentResolver resolver = context.getContentResolver();
+                    Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, queryTye, null, null,
+                            "sort_key COLLATE LOCALIZED ASC");
+                    if (phoneCursor == null || phoneCursor.getCount() == 0) {
+                        Toast.makeText(context, "未获得读取联系人权限 或 未获得联系人数据", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    int PHONES_NUMBER_INDEX = phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                    int PHONES_DISPLAY_NAME_INDEX = phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+                    int SORT_KEY_INDEX = phoneCursor.getColumnIndex("sort_key");
+                    int PHONEBOOK_LABEL = phoneCursor.getColumnIndex("phonebook_label");
+                    int PHOTO_ID = phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_ID);
+                    if (phoneCursor.getCount() > 0) {
+                       List<SortModel> mAllContactsList = new ArrayList<SortModel>();
+                        while (phoneCursor.moveToNext()) {
+                            String phoneNumber = phoneCursor.getString(PHONES_NUMBER_INDEX);
+                            if (TextUtils.isEmpty(phoneNumber))
+                                continue;
+                            // 头像id
+                            long photoId = phoneCursor.getLong(PHOTO_ID);
+                            String contactName = phoneCursor.getString(PHONES_DISPLAY_NAME_INDEX);
+                            String sortKey = phoneCursor.getString(SORT_KEY_INDEX);
+                            String book = phoneCursor.getString(PHONEBOOK_LABEL);
+                            SortModel sortModel = new SortModel(contactName, phoneNumber, sortKey);
+                            // //优先使用系统sortkey取,取不到再使用工具取
+                            // String sortLetters =
+                            // getSortLetterBySortKey(sortKey);
+                            // Log.i("main", "sortLetters:"+sortLetters);
+                            // if (sortLetters == null) {
+                            // sortLetters = getSortLetter(contactName);
+                            // }
+                            if (book == null) {
+                                book = "#";
+                            } else if (book.equals("#")) {
+                                book = "#";
+                            } else if (book.equals("")) {
+                                book = "#";
+                            }
+                            sortModel.sortLetters = book;
+                            sortModel.sortToken = parseSortKey(book);
+                            mAllContactsList.add(sortModel);
+                        }
+                    }
+                    phoneCursor.close();
+                   runOnUiThread(new Runnable() {
+                        public void run() {
+                            Collections.sort(mAllContactsList, pinyinComparator);
+                            adapter.updateListView(mAllContactsList);
+                        }
+                    });
+                } catch (Exception e) {
+                    Log.e("xbc", e.getLocalizedMessage());
+                }
+            }
+        }).start();
+    }
+
+
+
+    // String chReg="[^\\u4E00-\\u9FA5]";//除中文外的字符匹配
+    /**
+     * 解析sort_key,封装简拼,全拼
+     *
+     * @param sortKey
+     * @return
+     */
+    public SortToken parseSortKey(String sortKey) {
+        SortToken token = new SortToken();
+        if (sortKey != null && sortKey.length() > 0) {
+            // 其中包含的中文字符
+            String[] enStrs = sortKey.replace(" ", "").split(chReg);
+            for (int i = 0, length = enStrs.length; i < length; i++) {
+                if (enStrs[i].length() > 0) {
+                    // 拼接简拼
+                    token.simpleSpell += enStrs[i].charAt(0);
+                    token.wholeSpell += enStrs[i];
+                }
+            }
+        }
+        return token;
+    }
 }
